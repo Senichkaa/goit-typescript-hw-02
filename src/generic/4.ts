c26d540a664fb3423d7a843a74b123500e60a4b7
Senichkaa <super-kapanaiko406@ukr.net>
2023-10-20 18:44:57 +0300

    done hw

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface PropsOfT {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<PropsOfT> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
